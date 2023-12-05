a72db37a3d86cf68027a8555d3438663c63eb6f4
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-12-05 22:15:25 +1100

    removed social icons and props

--- client/src/components/sidebars/sidebar.jsx
+++ client/src/components/sidebars/sidebar.jsx
@@ -1,13 +1,12 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 //Side bar on homepage. functional links are future development
 function Sidebar(props) {
-  const { archives, description, social, title } = props;
+  const { archives, description, title } = props;
 
   return (
     <Grid item xs={12} md={6} sx={{ margin: "auto", padding: 2 }}>
@@ -60,13 +59,6 @@ Sidebar.propTypes = {
     })
   ).isRequired,
   description: PropTypes.string.isRequired,
-  social: PropTypes.arrayOf(
-    PropTypes.shape({
-      icon: PropTypes.elementType.isRequired,
-      name: PropTypes.string.isRequired
-    })
-  ).isRequired,
-  title: PropTypes.string.isRequired
 };
 
 export default Sidebar;
--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -5,9 +5,6 @@ import React, { useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 // import components
 import LatestComplaint from "../../components/latestComplaint";
@@ -45,11 +42,6 @@ const sidebar = {
     { title: "June 1999", url: "#" },
     { title: "May 1999", url: "#" },
     { title: "April 1999", url: "#" },
-  ],
-  social: [
-    { name: "GitHub", icon: GitHubIcon },
-    { name: "Twitter", icon: TwitterIcon },
-    { name: "Facebook", icon: FacebookIcon },
   ],
 };
 
