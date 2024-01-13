bdb51c56716d7076b0de60d601d0524f1ee8a398
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-14 01:17:14 +1100

    updated nav bar

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,70 +1,143 @@
 import React from "react";
-import "./navbar.css";
+import { Link, useLocation } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  useMediaQuery,
+  Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import PhoneIcon from "@mui/icons-material/Phone";
+import BookIcon from "@mui/icons-material/Book";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import AngryIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 
 import AuthService from "../../utils/auth";
 
-import { Link, useLocation } from "react-router-dom";
-
 const NavBar = () => {
+  const isMobile = useMediaQuery("(max-width:814px)");
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
   const currentPage = useLocation().pathname;
   const isAuthenticated = AuthService.loggedIn();
 
   const handleLogout = () => {
     AuthService.logout();
   };
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
+  const navLinks = [
+    { to: "/", text: "Home", icon: <HomeIcon /> },
+    { to: "/profile", text: "Profile", icon: <AccountCircleIcon /> },
+    { to: "/contact", text: "Contact", icon: <PhoneIcon /> },
+    { to: "/terms", text: "T&Cs", icon: <BookIcon /> },
+    { to: "/donate", text: "Donate", icon: <FavoriteIcon /> },
+    { to: "/complainred", text: "Complain Red", icon: <AngryIcon /> },
+  ];
+
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        {/* Link for "Home" */}
-        <Link to="/" className={currentPage === "/" ? "nav-link active" : "nav-link"} data-text="Home">
-          <i className="far fa fa-home"></i>
-          Home
-        </Link>
-        {/* Link for "Profile" */}
-        <Link to="/profile" className={currentPage === "/Profile" ? "nav-link active" : "nav-link"}>
-          <i className="far fa-user"></i>
-          Profile
-        </Link>
-        {/* Link for "Contact" */}
-        <Link to="/contact" className={currentPage === "/contact" ? "nav-link active" : "nav-link"}>
-          <i className="far fa fa-phone"></i>
-          Contact
-        </Link>
-        {/* Link for "Terms and Conditions" */}
-        <Link to="/terms" className={currentPage === "/terms" ? "nav-link active" : "nav-link"}>
-          <i className="far fa fa-book"></i>
-          T&Cs
-        </Link>
-        {/* Link for "Donate" */}
-        <Link to="/donate" className={currentPage === "/donate" ? "nav-link active" : "nav-link"}>
-          <i className="far fa-heart"></i>
-          Donate
-        </Link>
-        {/* Link for "ComplainRed" */}
-        <Link to="/complainred" className={currentPage === "/complainred" ? "nav-link active" : "nav-link"}>
-          <i className="far fa-angry"></i>
-          Complain Red
-        </Link>
-
-        {/* Link for "Signup/Signin" */}
-        {/* Conditional rendering for "Sign-up/Sign-in" or "Logout" */}
-        {isAuthenticated ? (
-          // Render Logout button if user is authenticated
-          <button onClick={handleLogout} className={isAuthenticated ? "nav-link" : "nav-link-hidden"}>
-            <i className="far fa-user"></i> Logout
-          </button>
-
-        ) : (
-          // Render Login button if user is not authenticated
-          <button id="signup">
-            <Link to="/SignUp" className={currentPage === "/signup" ? "nav-link active" : "nav-link"}>
-              <i className="far fa-user-circle"></i>
-            SIGN UP/IN
+    <>
+      <AppBar position="static" sx={{ backgroundColor: "black" }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          {isMobile ? (
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={toggleDrawer(true)}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            // Render desktop buttons
+            navLinks.map((link) => (
+              <Link to={link.to} className="nav-link" key={link.text}>
+                <Button
+                  color="inherit"
+                  sx={{
+                    color: "white",
+                    "&:hover": {
+                      backgroundColor: "blue",
+                    },
+                  }}
+                >
+                  {link.icon && <Box sx={{ marginRight: 1 }}>{link.icon}</Box>}
+                  {link.text}
+                </Button>
+              </Link>
+            ))
+          )}
+          {isAuthenticated ? (
+            <Button
+              onClick={handleLogout}
+              color="inherit"
+              sx={{
+                color: "white",
+                "&:hover": {
+                  backgroundColor: "purple",
+                },
+              }}
+            >
+              Logout
+            </Button>
+          ) : (
+            <Link
+              to="/signup"
+              className={
+                currentPage === "/signup" ? "nav-link active" : "nav-link"
+              }
+            >
+              <Button
+                color="inherit"
+                sx={{
+                  color: "white",
+                  "&:hover": {
+                    backgroundColor: "purple",
+                  },
+                }}
+              >
+                Sign Up/In
+              </Button>
             </Link>
-          </button>
-        )}
-      </div>
-    </nav>
+          )}
+        </Toolbar>
+      </AppBar>
+      {isMobile && (
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+          <List>
+            {navLinks.map((link) => (
+              <Link
+                to={link.to}
+                className="nav-link"
+                key={link.text}
+                onClick={toggleDrawer(false)}
+              >
+                <ListItem button>
+                  <ListItemText primary={link.text} />
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Drawer>
+      )}
+    </>
   );
 };
 
